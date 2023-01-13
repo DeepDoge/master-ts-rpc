464e657d9be47f77e9c323a3e1d2efe0aa225d7d
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-01-13 11:13:03 +0000

    ...

--- library/types/index.ts
+++ library/types/index.ts
@@ -1,6 +1,6 @@
 import type { z } from "zod"
 
-export interface RpcFunction<Z extends Zod.ZodObject<any> = Zod.ZodObject<any>, F extends (args: z.infer<Z>) => Promise<any> = (args: z.infer<Z>) => Promise<any>>
+export interface RpcFunction<Z extends Zod.ZodObject<any> = Zod.ZodObject<any>, F extends (args: z.infer<Z>) => Promise<any> = (args: any) => Promise<any>>
 {
     zod: Z
     rpc: F
