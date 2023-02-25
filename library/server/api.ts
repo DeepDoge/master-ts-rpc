f5ba2beb43b5f7992afb0b12d86f77b3c34a7123
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-02-25 11:28:25 +0000

    ...

--- library/server/api.ts
+++ library/server/api.ts
@@ -4,9 +4,9 @@ export type Rpcs = Record<string, Rpc<any, any>>
 export type RpcHandler<Functions extends Rpcs> = ReturnType<typeof createRpcHandler<Functions>>
 export function createRpcHandler<Functions extends Rpcs>(functions: Functions) {
 	const handler = async <K extends keyof Functions>(data: { key: K; args: Parameters<Functions[K]["parser"]> }) => {
-		const { parser, rpc } = functions[data.key] as Functions[K]
+		const { parser, func } = functions[data.key] as Functions[K]
 		const validated = parser(data.args)
-		return (await rpc(...validated)) ?? null
+		return (await func(...validated)) ?? null
 	}
 
 	return handler
