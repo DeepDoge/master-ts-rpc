92c99668e3de22b12883f2934736007a5c171e71
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-02-27 12:28:45 +0000

    ...

--- library/server/create.ts
+++ library/server/create.ts
@@ -1,17 +1,17 @@
 if (typeof window !== "undefined") throw new Error("RPC should only be created on the server side.")
 
-export type RpcInputParser<Args extends unknown[]> = {
+export type RpcInputParser<Args extends readonly unknown[]> = {
 	(input: unknown[]): Args
 }
-export type RpcFunction<Args extends unknown[], Return> = {
+export type RpcFunction<Args extends readonly unknown[], Return> = {
 	(...args: Args): Return
 }
-export type Rpc<Args extends any[], Return> = {
+export type Rpc<Args extends readonly any[], Return> = {
 	parser: RpcInputParser<Args>
 	func: RpcFunction<Args, Return>
 }
 
-export function createRpc<Args extends unknown[]>(parser: RpcInputParser<Args>) {
+export function createRpc<Args extends readonly unknown[]>(parser: RpcInputParser<Args>) {
 	return {
 		function<Return>(func: RpcFunction<Args, Return>): Rpc<Args, Return> {
 			return {
